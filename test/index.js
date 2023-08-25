738756faf5ecc77168352f3480e9f6154d12178a
Alexandra <alexandra.maranga@gmail.com>
2023-08-25 15:40:40 +0100

    fjdklfjl'fw

--- test/index.js
+++
@@ -1,7 +0,0 @@
-const myBtn = document.getElementById("myBtn");
-
-myBtn.addEventListener("click", function (e) {
-  //   const name = prompt("What is your name?");
-  console.log(name);
-  document.body.innerHTML = "<h1> Welcome! " + name + "<h1/>";
-});
